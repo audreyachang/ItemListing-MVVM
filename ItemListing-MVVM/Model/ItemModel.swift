078c9ccf3bf849ef04179ffb32e1d9afd799afe6
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2022-09-10 16:21:51 +0700

    Added coding keys for Item Data Model
    Defined coding keys for Item Data Model
    Added init to map data decoded from JSON data retrieved from API to defined data model

--- ItemListing-MVVM/Model/ItemModel.swift
+++ ItemListing-MVVM/Model/ItemModel.swift
@@ -16,3 +16,23 @@ struct Item{
     let itemRating: Float
 }
 
+extension Item: Decodable{
+    enum CodingKeys: String, CodingKey{
+        case itemId = "id"
+        case itemName = "title"
+        case itemDesc = "description"
+        case itemImage = "image"
+        case itemPrice = "price"
+        case itemRating = "rating"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        itemId = try container.decode(Int.self, forKey: .itemId)
+        itemName =  try container.decode(String.self, forKey: .itemName)
+        itemDesc =  try container.decode(String.self, forKey: .itemDesc)
+        itemImage = try container.decode(String.self, forKey: .itemImage)
+        itemPrice = try container.decode(Float.self, forKey: .itemPrice)
+        itemRating = try container.decode(Float.self, forKey: .itemRating)
+    }
+}
