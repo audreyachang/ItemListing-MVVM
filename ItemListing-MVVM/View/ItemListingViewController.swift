cf1e1ad5134eea24da16743a7916e3a5291400ec
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2022-09-12 09:20:45 +0700

    Added Labels to Item Listing Display
    Added labels to display message if unable to retrieve data
    Changed placeholder data in Item Placeholder
    Added auto layout constraints on newly added labels

--- ItemListing-MVVM/View/ItemListingViewController.swift
+++ ItemListing-MVVM/View/ItemListingViewController.swift
@@ -10,19 +10,26 @@ import UIKit
 class ItemListingViewController: UIViewController {
 
     @IBOutlet weak var itemListingTable: UITableView!
+    @IBOutlet weak var emptyLabel: UILabel!
+    @IBOutlet weak var emptyTitle: UILabel!
     
+    var placeholderItem: Item = ItemPlaceholder.getItemPlaceholder()
     var products: [Item] = []
     var displayedProducts: [Item] = []
     let networkManager = NetworkManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emptyLabel.text = ""
+        emptyTitle.text = ""
+        
         itemListingTable.dataSource = self
         itemListingTable.delegate = self
         
         itemListingTable.register(UINib.init(nibName: "ItemListingCell", bundle: nil), forCellReuseIdentifier: "ItemListingCell")
         itemListingTable.separatorColor = UIColor.clear
-        
+
         networkManager.getProducts { result in
             switch result{
             case .success(let products):
@@ -34,9 +41,12 @@ class ItemListingViewController: UIViewController {
                     self.itemListingTable.reloadData()
                 }
             case.failure(let error):
+                self.emptyData()
                 print(error.localizedDescription)
             }
         }
+        
+        
         
     }
     
@@ -45,16 +55,24 @@ class ItemListingViewController: UIViewController {
 extension ItemListingViewController: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return displayedProducts.count
+        if products.count != 0{
+            return displayedProducts.count
+        }else{
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = itemListingTable.dequeueReusableCell(withIdentifier: "ItemListingCell", for: indexPath)as! ItemListingCell
+        if products.count != 0{
+        emptyTitle.isHidden = true
+        emptyLabel.isHidden = true
         cell.itemName.text = products[indexPath.row].title
         cell.itemDesc.text = products[indexPath.row].itemDesc
         cell.itemPrice.text = "$ " + String(products[indexPath.row].price)
         cell.itemRating.text = "★" + String(products[indexPath.row].rating.rate)
         cell.itemImage.loadImage(imageURL: URL(string: products[indexPath.row].image)!)
+        }
         return cell
     }
     
@@ -77,7 +95,7 @@ extension ItemListingViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let sectionIndex = itemListingTable.numberOfSections - 1
         let lastRow = itemListingTable.numberOfRows(inSection: sectionIndex) - 1
-        if indexPath.section == sectionIndex && indexPath.row == lastRow {
+        if indexPath.section == sectionIndex && indexPath.row == lastRow && products.count > 0{
             let loadSpinner = UIActivityIndicatorView(style: .medium)
             if displayedProducts.count < 20 {
                 loadSpinner.frame = CGRect(x: 0.0, y: 0.0, width: itemListingTable.frame.width, height: 40)
@@ -89,6 +107,20 @@ extension ItemListingViewController: UITableViewDelegate, UITableViewDataSource{
         }
     }
     
+}
+
+extension ItemListingViewController{
+    func emptyData(){
+        if products.isEmpty{
+            emptyTitle.isHidden = false
+            emptyLabel.isHidden = false
+            emptyTitle.text = placeholderItem.title
+            emptyLabel.text = placeholderItem.itemDesc
+        }else{
+            emptyTitle.isHidden = true
+            emptyLabel.isHidden = true
+        }
+    }
 }
 
 
--- ItemListing-MVVM/ViewModel/ItemListingViewModel.swift
+++ ItemListing-MVVM/ViewModel/ItemListingViewModel.swift
@@ -13,7 +13,7 @@ struct ItemPlaceholder{
     let rating: Rating
     
     static func getItemPlaceholder()-> Item{
-        return Item(id: 0, title: "Item Placeholder", itemDesc: "There are no items available", image: "", price: 0.00, rating: Rating(rate: 0.00, count: 0))
+        return Item(id: 0, title: "Unable to get Data", itemDesc: "It seems that there's a problem,\n try again later", image: "", price: 0.00, rating: Rating(rate: 0.00, count: 0))
     }
 }
 
